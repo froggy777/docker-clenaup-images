28b7bff69ac5589da1218774fbb804e891b817e2
froggy777 <phen1x07@mail.ru>
2018-08-10 12:06:15 +0300

    Delete comments

--- get_images.py
+++ get_images.py
@@ -35,7 +35,6 @@ def print_old_tags(tag_number_list):
             tag_number = sorted(tag_number)
             len_tag_number = len(tag_number)
             if len_tag_number > 1:
-#                print images + " : " + str(tag_name) + " . " + str(tag_number)
                 current_tag_count = 0
                 for tag in tag_number:
                     current_tag_count +=1
@@ -45,7 +44,6 @@ def print_old_tags(tag_number_list):
 
 
 def sort_tag(images_and_tags_list ):
-#    print "sorting " + str(images_and_tags_list) + "\n"
     images_tags_number_list = {}
     for key, values in images_and_tags_list.items():
         values = sorted(values)
@@ -60,7 +58,6 @@ def sort_tag(images_and_tags_list ):
             output_color = 'green'
             if (tags_count != 1)and(current_tag +1) != tags_count:
                 output_color = 'red'
-#            print  colored ( str(key)+ ":" + str(value),output_color)
             current_tag += 1
             dot_position = str(value).rfind('.')
             if dot_position != -1:
@@ -73,9 +70,7 @@ def sort_tag(images_and_tags_list ):
                 tag_number_list.append(tag_number)
                 tag_name_list[tag_name] = (tag_number_list)
                 images_tags_number_list[key] = tag_name_list
-#                print colored(str(key), 'blue') + " : " +colored(tag_name,'red') + " . " + colored(tag_number,'green')
-#                  if str(key) == 'sfreydin/test_image_1':
-#                      print tag_number_list
+
     print_old_tags(images_tags_number_list)
 
     return images_tags_number_list
